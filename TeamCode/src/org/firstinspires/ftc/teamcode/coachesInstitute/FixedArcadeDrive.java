6a9b78f2e8aadd17996a49245da6fe65d6e00d0e
Alan Smith <alan@randomsmiths.com>
2022-07-28 16:56:05 -0400

    Pulled out SetPowers

--- TeamCode/src/org/firstinspires/ftc/teamcode/coachesInstitute/FixedArcadeDrive.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/coachesInstitute/FixedArcadeDrive.java
@@ -5,40 +5,45 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(group="@CoachesInstitute")
+@TeleOp(group = "@CoachesInstitute")
 public class FixedArcadeDrive extends OpMode {
-   DcMotor leftMotor;
-   DcMotor rightMotor;
+    DcMotor leftMotor;
+    DcMotor rightMotor;
 
-   @Override
-   public void init() {
-       leftMotor = hardwareMap.get(DcMotor.class, "left_motor");
-       rightMotor = hardwareMap.get(DcMotor.class, "right_motor");
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.get(DcMotor.class, "left_motor");
+        rightMotor = hardwareMap.get(DcMotor.class, "right_motor");
 
-       leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-       rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-       leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-   }
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
 
-   @Override
-   public void loop() {
-       double forward = -gamepad1.left_stick_y;
-       double right = gamepad1.left_stick_x;
-       double leftSpeed = forward + right;
-       double rightSpeed = forward - right;
+    private void setPowers(double leftSpeed, double rightSpeed) {
+        double maxSpeed = 1.0;
+        maxSpeed = Math.max(maxSpeed, Math.abs(leftSpeed));
+        maxSpeed = Math.max(maxSpeed, Math.abs(rightSpeed));
 
-       double maxSpeed = 1.0;
-       maxSpeed = Math.max(maxSpeed, Math.abs(leftSpeed));
-       maxSpeed = Math.max(maxSpeed, Math.abs(rightSpeed));
+        leftSpeed = leftSpeed / maxSpeed;
+        rightSpeed = rightSpeed / maxSpeed;
 
-       leftSpeed = leftSpeed / maxSpeed;
-       rightSpeed = rightSpeed / maxSpeed;
-       
-       leftMotor.setPower(leftSpeed);
-       rightMotor.setPower(rightSpeed);
-      
-       telemetry.addData("Left", leftSpeed);
-       telemetry.addData("Right", rightSpeed);
-   }
+        leftMotor.setPower(leftSpeed);
+        rightMotor.setPower(rightSpeed);
+
+        telemetry.addData("Left", leftSpeed);
+        telemetry.addData("Right", rightSpeed);
+    }
+
+    @Override
+    public void loop() {
+        double forward = -gamepad1.left_stick_y;
+        double right = gamepad1.left_stick_x;
+        double leftSpeed = forward + right;
+        double rightSpeed = forward - right;
+
+        setPowers(leftSpeed, rightSpeed);
+    }
+
 }
 

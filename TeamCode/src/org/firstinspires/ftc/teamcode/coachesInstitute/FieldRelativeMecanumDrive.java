9897f8ebb7c8a9ac80971c5fad97eadfc1ed85fe
Alan Smith <alan@randomsmiths.com>
2022-07-28 16:56:58 -0400

    Added a field relative example

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/coachesInstitute/FieldRelativeMecanumDrive.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.coachesInstitute;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(group="@CoachesInstitute")
+public class FieldRelativeMecanumDrive extends OpMode {
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
+    BNO055IMU imu;
+
+    @Override
+    public void init() {
+        frontLeftMotor= hardwareMap.dcMotor.get("front_left_motor");
+        frontRightMotor = hardwareMap.dcMotor.get("front_right_motor");
+        backLeftMotor = hardwareMap.dcMotor.get("back_left_motor");
+        backRightMotor = hardwareMap.dcMotor.get("back_right_motor");
+
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        imu.initialize(parameters);
+    }
+
+    // Thanks to FTC16072 for sharing this code!!
+    public void drive(double forward, double right, double rotate) {
+        double leftFrontPower = forward + right + rotate;
+        double rightFrontPower = forward - right - rotate;
+        double rightRearPower = forward + right - rotate;
+        double leftRearPower = forward - right + rotate;
+        double maxPower = 1.0;
+        double maxSpeed = 1.0;  // make this slower for outreaches
+
+        maxPower = Math.max(maxPower, Math.abs(leftFrontPower));
+        maxPower = Math.max(maxPower, Math.abs(rightFrontPower));
+        maxPower = Math.max(maxPower, Math.abs(rightRearPower));
+        maxPower = Math.max(maxPower, Math.abs(leftRearPower));
+
+
+        frontLeftMotor.setPower(maxSpeed * (leftFrontPower/maxPower));
+        frontRightMotor.setPower(maxSpeed * (rightFrontPower/maxPower));
+        backLeftMotor.setPower(maxSpeed * (leftRearPower/maxPower));
+        backRightMotor.setPower(maxSpeed * (rightRearPower/maxPower));
+    }
+
+    private void driveFieldRelative(double forward, double right, double rotate){
+        Orientation orientation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
+        // convert to polar
+        double theta = Math.atan2(forward, right);
+        double r = Math.hypot(right, forward);
+        // rotate angle
+        theta = AngleUnit.normalizeRadians(theta - orientation.firstAngle);
+
+        // convert back to cartesian
+        double newForward = r * Math.sin(theta);
+        double newRight = r * Math.cos(theta);
+        
+        drive(newForward, newRight, rotate);
+    }
+
+    @Override
+    public void loop() {
+        driveFieldRelative(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+    }
+
+}
